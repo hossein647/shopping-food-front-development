b486c78d1c3937673a5df37dfd6e7189e092583c
hossein.hedyeh@gmail.com <hossein.hedyeh@gmail.com>
2022-06-29 00:40:31 +0430

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   url: 'https://api.hosseinnejaty.ir'
   // url: 'https://shopping-food-node-app.iran.liara.run'
-  // url: 'https://finn.iran.liara.ir:3479' 2454
+  // url: 'https://finn.iran.liara.ir:3479'
   
 };
