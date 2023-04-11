8c9a4c498c5d6135f8bb6f2d903087b9fa570f5c
hossein.hedyeh@gmail.com <hossein.hedyeh@gmail.com>
2023-04-11 18:50:26 +0330

    add url with subdomin to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: 'https://api.hosseinnejaty.ir'
+  url: 'https://api.order-food.hosseinnejaty.ir' || 'http://api.order-food.hosseinnejaty.ir'
   // url: 'https://shopping-food-node-app.iran.liara.run'
   // url: 'https://shopping-food-node-app.iran.liara.run'
 };
