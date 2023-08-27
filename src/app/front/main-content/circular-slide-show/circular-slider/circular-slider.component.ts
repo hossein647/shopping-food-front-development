03cda6a6ed45e2cbffdf9079a30b8b4f2d3b0910
hossein.hedyeh@gmail.com <developandroo@gmail.com>
2023-08-27 13:45:23 +0330

    change time run calculatePositionImage_3 with 1 and 2 with 4 in setPostionImageCircularSlider method

--- src/app/front/main-content/circular-slide-show/circular-slider/circular-slider.component.ts
+++ src/app/front/main-content/circular-slide-show/circular-slider/circular-slider.component.ts
@@ -22,9 +22,7 @@ export class CircularSliderComponent implements OnInit, AfterViewInit, OnDestroy
   @ViewChild('counter')                    counter: ElementRef;
   @ViewChild('title')                        title: ElementRef;
   @ViewChildren('imgCircular')         imgCircular: QueryList<ElementRef>;
-  @ViewChild('imgLinear')                imgLinear: QueryList<ElementRef>;
-  // @ViewChildren('imgLinearParent') imgLinearParent: QueryList<ElementRef>;
-  // @ViewChild('cover')                        cover: ElementRef;  
+  @ViewChild('imgLinear')                imgLinear: QueryList<ElementRef>;  
 
   
   linearFoodOnMoment = new Subject<{index: number, title: string}>();
@@ -148,7 +146,7 @@ export class CircularSliderComponent implements OnInit, AfterViewInit, OnDestroy
   getShowStateImages() {
     this.circularService.getShowStates().subscribe((res: any) => {   
       if (res?.foods.length > 0) this.foods = JSON.parse(JSON.stringify(res)).foods;
-     this.foods = this.getDefaultSuperFoods() as  any;
+      else  this.foods = this.getDefaultSuperFoods() as  any;
     })
   }
 
@@ -160,14 +158,13 @@ export class CircularSliderComponent implements OnInit, AfterViewInit, OnDestroy
       images.forEach((image: any, index: number) => {
         index++
         const foodImage = image.nativeElement;
-        if (index === 1) this.calculatePositionImage_1(slider, foodImage, this.outlinWidthSlider);
-        if (index === 2) this.calculatePositionImage_2(slider, foodImage, this.outlinWidthSlider);
-        if (index === 3) this.calculatePositionImage_3(slider, foodImage, this.outlinWidthSlider);
-        if (index === 4) this.calculatePositionImage_4(slider, foodImage, this.outlinWidthSlider);
+        if (index === 1) this.calculatePositionImage_3(slider, foodImage, this.outlinWidthSlider);
+        if (index === 2) this.calculatePositionImage_4(slider, foodImage, this.outlinWidthSlider);
+        if (index === 3) this.calculatePositionImage_1(slider, foodImage, this.outlinWidthSlider);
+        if (index === 4) this.calculatePositionImage_2(slider, foodImage, this.outlinWidthSlider);
       })
       this.renderer.setStyle(this.counter.nativeElement, 'transform', 'translateX(0)');
       this.renderer.setStyle(this.title.nativeElement, 'transform', 'translateX(0)');
-      // this.renderer.setStyle(this.linearSlider.nativeElement, 'transform', 'translateX(0)');
     })
   }
 
@@ -183,50 +180,33 @@ export class CircularSliderComponent implements OnInit, AfterViewInit, OnDestroy
     this.clickSliding$.next(1)
   }
 
-  setPositionCoverImage() {
-    // this.imgLinearParent.changes.subscribe(parents => {
-    //   console.log(parents);
-      
-    //   parents.forEach((parent: ElementRef, index: number) => {
-    //     console.log(parent);
-        
-    //     index++;        
-        this.linearFoodOnMoment.subscribe(food => {   
-          // if (food.index === index) {            
-            // this.calculatePositonCoverImage(parent);
-            this.foodIndex = food.index + '#';
-            this.foodTitle = food.title;
-          // }
-        })
-      // })
-    // })
+  setPositionCoverImage() {   
+    this.linearFoodOnMoment.subscribe(food => {
+        this.foodIndex = food.index + '#';
+        this.foodTitle = food.title;
+    })
   }
 
-  calculatePositonCoverImage(parent: ElementRef) {
-    const linearSlider = this.linearSlider.nativeElement.getBoundingClientRect();
-    // const coverPosition = this.cover.nativeElement.getBoundingClientRect();
-    const ParentPosition = parent.nativeElement.getBoundingClientRect();
-    // const left = coverPosition.left - (coverPosition.left - ParentPosition.left) - linearSlider.left;
-
-    // this.renderer.addClass(this.cover.nativeElement, 'selected')
-    // this.renderer.setStyle(this.cover.nativeElement, 'width', `${ParentPosition.width}px`)
-    // this.renderer.setStyle(this.cover.nativeElement, 'height', `${ParentPosition.height}px`)
-    // this.renderer.setStyle(this.cover.nativeElement, 'left', `${left}px`);
-  }
 
   emitDateSelectedImage(degree: number) { 
     let result: string = `${degree / 360}`;
     const num: number = Math.sign(degree / 360);
-
+    
     if (+result < 0) result = '-' + result.slice(-3);
     
-    if ((result.endsWith('.25') && num === 1) || (result.endsWith('-.75') && num === -1))
+    if ((result.endsWith('.25') && num === 1) || (result.endsWith('-.75') && num === -1)) {              
       this.linearFoodOnMoment.next({ index: 1, title: this.foods[0]?.name });
+    }
+
     if (result.endsWith('.5')) this.linearFoodOnMoment.next({ index: 2, title: this.foods[1]?.name });
-    if ((result.endsWith('.75') && num === 1) || (result.endsWith('-.25')  && num === -1)) 
+
+    if ((result.endsWith('.75') && num === 1) || (result.endsWith('-.25')  && num === -1)) {
       this.linearFoodOnMoment.next({ index: 3, title: this.foods[2]?.name });
-    if (Number.isInteger(degree / 360)) 
+    }
+
+    if (Number.isInteger(degree / 360)) {
       this.linearFoodOnMoment.next({ index: 4, title: this.foods[3]?.name });
+    }
   }
 
   setPositionStyle(image: HTMLElement, rotate: number, top?: string) {
@@ -292,91 +272,35 @@ export class CircularSliderComponent implements OnInit, AfterViewInit, OnDestroy
   getDefaultSuperFoods() {
     return [
       {
-        "_id": "62d048ebd9d42f68727d4a5e",
+        "_id": "62caa8344fb629cd46a35361",
         "comment": [
-
-        ],
-        "rating": [
-
-        ],
-        "reserved": {
-          "original": 1658085889976.0,
-          "local": "شنبه ۲۵ تیر ۱۴۰۱ "
-        },
-        "state": "کامل",
-        "super": true,
-        "name": "خوراک ماهی قزل آلا کبابی",
-        "description": "۴۰۰ گرم ماهی، نان لواش، دورچین",
-        "category": "ماهی",
-        "shop": "لاله",
-        "subFood": "ماهی",
-        "price": 157000,
-        "copon": null,
-        "image": "main-qimg-3011cd9b472121fd69238b0e640d4c38-4e9f.jpg",
-        "imageId": "64ea04e09d81489e36815994",
-        "userId": "62cc46ae400cb71607efaf27",
-        "createdAt": "2022-07-14T16:48:43.425+0000",
-        "updatedAt": "2023-08-26T14:03:31.467+0000",
-        "__v": 0
-      },
-      {
-        "_id": "62caddaf4fb629cd46a353be",
-        "comment": [
-
+          "62caa8654fb629cd46a35371"
         ],
         "rating": [
           {
             "userId": "62c1b8b388924809102e8e6b",
-            "rate": 1
+            "rate": 5
           }
         ],
         "reserved": {
-          "original": 1657999458057.0,
-          "local": "جمعه ۱ اردیبهشت ۱۴۰۲ "
+          "original": 1693166358482.0,
+          "local": "یکشنبه ۵ شهریور ۱۴۰۲ "
         },
-        "state": "کامل",
-        "super": false,
-        "name": "چلو کباب وزیری",
-        "description": "۲۰۰ گرم جوجه کباب، ۱۰۰ گرم کباب کوبیده، برنج ایرانی، فلفل، ترشی کلم، لیمو، نارنج، گوجه کبابی",
+        "state": "تایید",
+        "super": true,
+        "name": "چلو کباب کوبیده",
+        "description": "۲ سیخ ۱۰۰ گرمی مخلوط گوشت گوساله و گوسفندی، برنج ایرانی، فلفل، ترشی کلم، لیمو، نارنج، گوجه کبابی",
         "category": "کباب گوشت",
         "shop": "آزادی",
         "subFood": "کباب گوشت",
-        "price": 75900,
-        "copon": null,
-        "image": "1580728211618-e9a7.jpg",
-        "imageId": "64e5cb469d81489e36815824",
+        "price": 57800,
+        "copon": 7900,
+        "image": "576-5762035_koobideh-kabob-kebab-png-top-view-transparent-png-85106-ac89.png",
+        "imageId": "64e4cd349d81489e36815553",
         "userId": "62c1b8ec88924809102e8e70",
-        "createdAt": "2022-07-10T14:09:51.734+0000",
-        "updatedAt": "2023-08-23T09:03:34.381+0000",
-        "__v": 0
-      },
-      {
-        "_id": "62cc061d486bbef38c06a717",
-        "comment": [
-
-        ],
-        "rating": [
-
-        ],
-        "reserved": {
-          "original": 1657999467646.0,
-          "local": "سه‌شنبه ۲۲ فروردین ۱۴۰۲ "
-        },
-        "state": "کامل",
-        "super": true,
-        "name": "پیتزا مخلوط",
-        "description": "خمیر پیتزا کلاسیک ۳۰ سانتی متری، ژامبون گوشت و مرغ، هات داگ فرانکفورتر، قارچ، زیتون، گوجه فرنگی، فلفل دلمه‌ ای، پنیر موزارلا، سس مخصوص، سیب زمینی سرخ شده",
-        "category": "پیتزا",
-        "shop": "ستارخان",
-        "subFood": "پرطرفدار",
-        "price": 112000,
-        "copon": 128000,
-        "image": "pizza-da2a.png",
-        "imageId": "64ea0def9d81489e36816006",
-        "userId": "62cbfaf1486bbef38c06a649",
-        "createdAt": "2022-07-11T11:14:37.648+0000",
-        "updatedAt": "2023-08-26T14:37:03.409+0000",
-        "__v": 0
+        "createdAt": "2022-07-10T10:21:40.157+0000",
+        "updatedAt": "2023-08-26T19:59:18.484+0000",
+        
       },
       {
         "_id": "62cb1f81781babfa344d67a2",
@@ -394,11 +318,11 @@ export class CircularSliderComponent implements OnInit, AfterViewInit, OnDestroy
           }
         ],
         "reserved": {
-          "original": 1657999458057.0,
-          "local": "جمعه ۱ اردیبهشت ۱۴۰۲ "
+          "original": 1693166574248.0,
+          "local": "یکشنبه ۵ شهریور ۱۴۰۲ "
         },
-        "state": "کامل",
-        "super": false,
+        "state": "تایید",
+        "super": true,
         "name": "چلو کباب بختیاری",
         "description": "۱۲۰ گرم کباب برگ گوشت گوسفندی، ۱۲۵ گرم شنیسل مرغ، برنج ایرانی، فلفل، ترشی کلم، لیمو، نارنج، گوجه کبابی",
         "category": "کباب گوشت",
@@ -410,8 +334,64 @@ export class CircularSliderComponent implements OnInit, AfterViewInit, OnDestroy
         "imageId": "64e5cb469d81489e36815826",
         "userId": "62c1b8ec88924809102e8e70",
         "createdAt": "2022-07-10T18:50:41.748+0000",
-        "updatedAt": "2023-08-23T09:04:02.308+0000",
-        "__v": 0
+        "updatedAt": "2023-08-26T20:02:54.249+0000",
+        
+      },
+      {
+        "_id": "62cc061d486bbef38c06a717",
+        "comment": [
+
+        ],
+        "rating": [
+
+        ],
+        "reserved": {
+          "original": 1693166085524.0,
+          "local": "یکشنبه ۵ شهریور ۱۴۰۲ "
+        },
+        "state": "تایید",
+        "super": true,
+        "name": "پیتزا مخلوط",
+        "description": "خمیر پیتزا کلاسیک ۳۰ سانتی متری، ژامبون گوشت و مرغ، هات داگ فرانکفورتر، قارچ، زیتون، گوجه فرنگی، فلفل دلمه‌ ای، پنیر موزارلا، سس مخصوص، سیب زمینی سرخ شده",
+        "category": "پیتزا",
+        "shop": "ستارخان",
+        "subFood": "پرطرفدار",
+        "price": 112000,
+        "copon": 128000,
+        "image": "pizza-da2a.png",
+        "imageId": "64ea0def9d81489e36816006",
+        "userId": "62cbfaf1486bbef38c06a649",
+        "createdAt": "2022-07-11T11:14:37.648+0000",
+        "updatedAt": "2023-08-26T19:54:45.525+0000",
+        
+      },
+      {
+        "_id": "62d048ebd9d42f68727d4a5e",
+        "comment": [
+
+        ],
+        "rating": [
+
+        ],
+        "reserved": {
+          "original": 1693166093250.0,
+          "local": "یکشنبه ۵ شهریور ۱۴۰۲ "
+        },
+        "state": "تایید",
+        "super": true,
+        "name": "خوراک ماهی قزل آلا کبابی",
+        "description": "۴۰۰ گرم ماهی، نان لواش، دورچین",
+        "category": "ماهی",
+        "shop": "لاله",
+        "subFood": "ماهی",
+        "price": 157000,
+        "copon": null,
+        "image": "main-qimg-3011cd9b472121fd69238b0e640d4c38-4e9f.jpg",
+        "imageId": "64ea04e09d81489e36815994",
+        "userId": "62cc46ae400cb71607efaf27",
+        "createdAt": "2022-07-14T16:48:43.425+0000",
+        "updatedAt": "2023-08-26T19:56:13.904+0000",
+        
       }
     ]
   }
