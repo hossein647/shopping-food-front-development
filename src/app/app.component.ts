bd6e2cd33ee9882b29bde207160d76dd30ff031e
hossein.hedyeh@gmail.com <developandroo@gmail.com>
2023-08-30 17:57:59 +0330

    remove title property and import not used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,4 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'اپلیکیشن سفارش غذا';
 }
